407dc773c36cda99a5c116b394ecdf6b54510451
Hansanghyeon <dandan9509@gmail.com>
2020-05-23 01:24:29 +0900

    Scaffolding: 플러그인 진입점, 플러그인 설명및 셋팅

---
+++ core-functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Neu dash
+ * 
+ * @package  Neu_dash
+ * @copyright Copyright(c) 2020, Hansanghyeon
+ * @license http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2 (GPL-2.0)
+ * 
+ * Plugin Name: Neumorphism style admin dashboard for Wordpress
+ * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Description: My First Plugin, Neumorphism wp admin dashboard
+ * Version: 1.0
+ * Author: Hansanghyeon
+ * Author URI: https://github.com/Hansanghyeon
+ * License: GPL2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: block-lab
+ * Domain Path: languages
+ */
+
